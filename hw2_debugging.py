0f0e63d6e5926b2c0f63c3ef0752947610b669a0
Brijesh Kumar <39380117+KoruptTinker@users.noreply.github.com>
2024-09-10 19:06:06 +0000

    Fix: Merge sort logic

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -50,17 +50,19 @@ def recombine(left_arr, right_arr):
     merged_arr = [None] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
             merged_arr[left_index + right_index] = left_arr[left_index]
-        else:
             left_index += 1
+        else:
             merged_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    for i in range(right_index, len(right_arr)):
-        merged_arr[left_index + right_index] = right_arr[i]
+    while(right_index < len(right_arr)):
+        merged_arr[left_index + right_index] = right_arr[right_index]
+        right_index += 1
 
-    for i in range(left_index, len(left_arr)):
-        merged_arr[left_index + right_index] = left_arr[i]
+    while(left_index < len(left_arr)):
+        merged_arr[left_index + right_index] = left_arr[left_index]
+        left_index += 1
 
     return merged_arr
 

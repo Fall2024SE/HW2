303319842c528a6f3b4272190cc95d04ae7ce63b
Brijesh Kumar <39380117+KoruptTinker@users.noreply.github.com>
2024-09-10 18:43:37 -0400

    Update rand.py

--- src/rand.py
+++ src/rand.py
@@ -2,6 +2,7 @@
 This module provides utility functions to generate random numbers
 """
 import secrets
+import random
 
 def random_array(arr):
     """
@@ -18,5 +19,6 @@ def random_array(arr):
         The modified list where each element is replaced with a random number from 1 to 20.
     """
     for i in range(len(arr)):
+        random.random()
         arr[i] = secrets.randbelow(21)
     return arr
